e615f1d422e65bc9089c714048f8d3418283c545
RyzewitchChicken <daft56punk@hotmail.com>
2020-06-16 17:32:45 -0500

    imagenes en productos, ofertas, links del footer, actualizacion de visualizacion de las ofertas

--- src/app/entidades/producto.ts
+++ src/app/entidades/producto.ts
@@ -3,12 +3,15 @@ export class Producto {
     nombre:string;
     descripcion:string;
     precio:number;
+    imagen:string;
 
-    constructor(id, descripcion, name, precio)
+
+    constructor(id, descripcion, name, precio,imagen)
     {
         this.codigo = id;
         this.nombre = name;
         this.descripcion = descripcion;
         this.precio = precio;
+        this.imagen=imagen;
     }
 }
